6329d1e2398ef65331084e21b1a2b20f1550e51e
Milan Musec <milan@musec.sk>
2019-11-19 08:26:55 +0100

    update

--- powershell/ZonkyNxt.ps1
+++ powershell/ZonkyNxt.ps1
@@ -1,21 +1,91 @@
+#requires 
 
-$z_cred=Get-Content -Path 'Zonky.pwd' | ConvertFrom-Json
 Add-Type -AssemblyName System.Web
-$uri = 'https://app.zonky.cz/api/oauth/authorize?client_id=mujrobot&redirect_uri=https://app.zonky.cz/api/oauth/code&response_type=code&scope=SCOPE_APP_BASIC_INFO+SCOPE_INVESTMENT_READ&state=opaque'
-$ie = New-Object -ComObject InternetExplorer.Application
-$ie.Visible=$false
-$ie.Navigate($uri)
-while ($ie.ReadyState -ne 4) {Start-Sleep -m 100}
-$ie.document.getElementById("email").value = ""
-$ie.document.getElementById("password").value =""
-$ie.Document.getElementById("login-form").submit()
-while ($ie.ReadyState -ne 4) {Start-Sleep -m 100}
-$uri_code = [uri]$ie.LocationURL
-$code=[System.Web.HttpUtility]::ParseQueryString(($uri_code).Query)['code']
-$ie.Stop()
-$ie.Quit()
 
-$code
+class zCredential {
+    [string]$email
+    [securestring]$password
+    zCredential() {}
+    zCredential([string]$Email,[securestring]$Password){
+        $this.email=$Email
+        $this.password=$Password
+    } 
+}
+class zApi {
+    hidden [string[]]$scope = 'SCOPE_APP_BASIC_INFO SCOPE_INVESTMENT_READ'
+    hidden [string]$redirect_uri = 'https://app.zonky.cz/api/oauth/code'
+    hidden [string]$user_agent = 'zonkyNXT/1.0 (https://github.com/MilanNXT/ZonkyNxt)'    
+    [string]$authorization_code
+}
+
+class zLogin {
+    #TODO: scopes
+    hidden [string]$login_uri = 'https://app.zonky.cz/api/oauth/authorize?client_id=mujrobot&redirect_uri=https://app.zonky.cz/api/oauth/code&response_type=code&scope=SCOPE_APP_BASIC_INFO+SCOPE_INVESTMENT_READ&state=opaque'
+    hidden [string]$pwd_file
+    hidden [zCredential]$zcredential
+    hidden [zApi]$api
+
+    hidden init() {
+        $this.init('ZonkyNxt.pwd')
+    }
+    hidden init([string]$PwdFile){
+        if (Test-Path -Path $PwdFile) {
+            $this.pwd_file=$PwdFile
+        } else {
+            $this.pwd_file='ZonkyNxt.pwd'
+        }
+        $tmp_cred = Get-Content -Path $this.pwd_file | ConvertFrom-Json
+        $this.init($tmp_cred.email,(ConvertTo-SecureString -String $tmp_cred.password -AsPlainText -Force))
+    }
+    hidden init([string]$Email,[securestring]$Password) {
+        $this.zcredential = [zCredential]::new($Email,$Password)
+        $this.api = [zApi]::new()
+    }
+    zLogin() {
+        $this.init()
+    }
+    zLogin([string]$PwdFile) {
+        $this.init($PwdFile)
+    }
+    zLogin([string]$Email,[securestring]$Password) {
+        $this.init($Email,$Password)
+    }
+    zLogin([string]$Email,[string]$PlainPassword) {
+        $this.init($Email,(ConvertTo-SecureString -String $PlainPassword -AsPlainText -Force))
+    }
+    [void] login() {
+        $ie = New-Object -ComObject InternetExplorer.Application
+        $ie.Visible=$false
+        $ie.Navigate($this.login_uri)
+        while ($ie.ReadyState -ne 4) {Start-Sleep -m 100}
+        $ie.document.getElementById("email").value = $this.zcredential.email
+        $ie.document.getElementById("password").value =""
+        $ie.Document.getElementById("login-form").submit()
+        while ($ie.ReadyState -ne 4) {Start-Sleep -m 100}
+        $uri_code = [uri]$ie.LocationURL
+        $this.api.authorization_code=[System.Web.HttpUtility]::ParseQueryString(($uri_code).Query)['code']
+        $ie.Stop()
+        $ie.Quit()        
+    }
+    [void] new_token() {
+        $uri = "https://api.zonky.cz/oauth/token"
+        $auth = 'mujrobot:mujrobot' 
+        $auth_b64 = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$auth"))
+        $Headers = @{
+            'Content-Type'='application/x-www-form-urlencoded'
+            'Authorization'="Basic $auth_b64"
+            'User-Agent' = $this.api_user_agent
+        }
+    
+        $body = "scope=$scope&grant_type=authorization_code&code=$($this.api_authorization_code)&redirect_uri=$this.api_redirect_uri"        
+        try {
+            $auth = Invoke-RestMethod -Method POST -Uri $URI -Headers $Headers -Body $Body  -UseBasicParsing
+        } catch {
+            $ex = $_
+            Write-Host $ex.Exception.Message
+        }    
+    }
+}
 
 class Crdential{
     [string]$access_token = ''
