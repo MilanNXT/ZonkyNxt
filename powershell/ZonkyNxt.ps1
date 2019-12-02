9af7883187aeed5ca6345402226c58ee3609b4a0
Milan Musec <milan@musec.sk>
2019-12-02 23:30:26 +0100

    updates

--- powershell/ZonkyNxt.ps1
+++ powershell/ZonkyNxt.ps1
@@ -1,12 +1,12 @@
 Set-StrictMode -Version latest
 Add-Type -AssemblyName System.Web
-. D:\OneDrive\Repos\ZonkyNxt\powershell\ClassZonkyNxt.ps1
+#. D:\OneDrive\Repos\ZonkyNxt\powershell\ClassZonkyNxt.ps1
+. D:\Repo\ZonkyNxt\powershell\ClassZonkyNxt.ps1
 
 $zonky = [ZonkyNxt]::new()
-$zonky.connect('D:\OneDrive\Repos\ZonkyNxt\ZonkyNxt.pwd')
+#$zonky.connect('D:\OneDrive\Repos\ZonkyNxt\ZonkyNxt.pwd')
+$zonky.connect('D:\Users\Milan\OneDrive\Repos\ZonkyNxt\ZonkyNxt.pwd')
 $zonky.GetMarketplace()
 $zonky.GetInvestments()
 #Remove-Variable zonky
-
-
 
